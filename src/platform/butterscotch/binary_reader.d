7ddd85beae6563c998ba542b0f4da7ef83b0f039
RainbowKappa <goldenethernetgames@gmail.com>
2026-06-10 20:07:01 -0500

    Delete src/platform/butterscotch directory

--- src/platform/butterscotch/binary_reader.d
+++
@@ -1,4 +0,0 @@
-src/butterscotch/binary_reader.o: src/butterscotch/binary_reader.c \
- src/butterscotch/binary_reader.h src/butterscotch/common.h \
- src/butterscotch/binary_utils.h src/butterscotch/utils.h \
- src/butterscotch/real_type.h
